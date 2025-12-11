4242fa24c1e93ed75c9fd136248d66bb5bfff6d7
Morrigan <ms.netty@inbox.ru>
2025-12-11 16:27:03 +0300

    добавлены пути для расписания

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import CustomSignupView, ProfileView, ScheduleCreateView, ScheduleListView, AvailableScheduleListView, book_appointment
+from .views import (
+                    CustomSignupView, ProfileView, ScheduleCreateView, 
+                    ScheduleListView, AvailableScheduleListView, 
+                    book_appointment, ScheduleRequestsView,
+                    confirm_request, cancel_request,
+                    MyAppointmentsView
+)
 
 app_name = 'users'
 
@@ -9,4 +15,8 @@ urlpatterns = [
     path('profile/schedules/add/', ScheduleCreateView.as_view(), name="admin_schedule_add"),
     path('profile/appointments/', AvailableScheduleListView.as_view(), name="available_schedule"),
     path('book/<int:slot_id>/', book_appointment, name='book_appointment'),
+    path('profile/schedules/requests/', ScheduleRequestsView.as_view(), name="schedule_requests"),
+    path('profile/schedules/requests/<int:pk>/confirm/', confirm_request, name="confirm_request"),
+    path('profile/schedules/requests/<int:pk>/cancel/', cancel_request, name="cancel_request"),
+    path('profile/my-appointments/', MyAppointmentsView.as_view(), name="my_appointments"),
 ]
