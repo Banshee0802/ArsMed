18fbfe961d8476d04a97619be469ab4bb5fa1ed0
Morrigan <ms.netty@inbox.ru>
2026-01-12 19:11:35 +0300

    добавление единой маски в поле номера телефона при регистрации

--- users/forms.py
+++ users/forms.py
@@ -14,11 +14,27 @@ class CustomSignupForm(SignupForm):
         widget=forms.RadioSelect
     )
     birth_date = forms.DateField(label='Дата рождения', widget=forms.DateInput(attrs={'type': 'date'}))
-    phone = forms.CharField(max_length=15, label="Номер телефона")
+    phone = forms.CharField(max_length=15, 
+                            widget=forms.TextInput(attrs={'id': 'id_phone'}),
+                            label="Номер телефона")
     consent_personal = forms.BooleanField(
         required=True,
         label="Я даю согласие на обработку персональных данных"
     )
+
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+       
+        digits = ''.join(c for c in phone if c.isdigit())
+ 
+        if len(digits) == 10:
+            digits = '7' + digits
+        elif len(digits) == 11 and digits.startswith('8'):
+            digits = '7' + digits[1:]
+        elif len(digits) != 11 or not digits.startswith('7'):
+            raise forms.ValidationError("Введите корректный российский номер телефона")
+
+        return '+' + digits
 
     def save(self, request):
         user = super().save(request)
